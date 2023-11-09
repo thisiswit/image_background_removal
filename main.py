b39cde789add541d681b05be313bd88abaab9808
thisiswit <zack_black18@live.com>
2023-11-09 11:26:59 -0300

    new functionality image url added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import inference
 import os
+import requests
+import urllib.request 
 from inference import x
 
-application = Flask(__name__) # initializing a flask app
+application = Flask(__name__)
 app=application
 UPLOAD_FOLDER = 'static/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -25,8 +27,11 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         urlImage = request.form['urlImage']
+        
+        if f and urlImage:
+            return render_template("index-duplicated.html")
 
-        if f:        
+        elif f:        
             filename = secure_filename(f.filename)
             print("**************************")
             print(filename)
@@ -37,20 +42,23 @@ def upload_file():
             print(x)
             return render_template("uploaded.html", display_detection=filename, fname=filename, x=x)
         
-        if urlImage:
-             return render_template("about.html")
-
-        return render_template("index.html")
+        elif urlImage:
+            fname = urlImage.split("/")[-1]
+            path = f"static/uploads/{fname}"
+            urllib.request.urlretrieve(urlImage, path)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+            inference.predict(filepath)
+            return render_template("uploaded.html", display_detection=path, fname=fname, x=x)
+        
+        return render_template("index-error.html")
 
 @app.route('/display/<filename>')
 def display_image(filename):
-	#print('display_image filename: ' + filename)
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
-# No caching at all for API endpoints.
+# Removendo o cache dos endpoints.
 @app.after_request
 def add_header(response):
-    # response.cache_control.no_store = True
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
