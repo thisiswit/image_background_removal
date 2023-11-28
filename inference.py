b3e52eb86120446e0e79430b905b23a1241a7821
thisiswit <zack_black18@live.com>
2023-11-27 21:39:07 -0300

    cleaning code, adding comments e small changes

--- inference.py
+++ inference.py
@@ -11,17 +11,17 @@ x = datetime.datetime.now()
 model_path = "pretrained/modnet.onnx"
 output_path = f"static/results/output_{x}.png"
 
+
 def predict(image_path):
     if not os.path.exists(image_path):
-        print('Cannot find input path: {0}'.format(image_path))
+        print(f'Não foi possivel encontrar o caminho do input: {image_path}')
         exit()
     if not os.path.exists(model_path):
-        print('Cannot find model path: {0}'.format(model_path))
+        print(f'Não foi possivel encontrar o caminho do modelo: {model_path}')
         exit()
-
     ref_size = 512
 
-    # Get x_scale_factor & y_scale_factor to resize image
+
     def get_scale_factor(im_height, im_width, ref_size):
 
         if max(im_height, im_width) < ref_size or min(im_height, im_width) > ref_size:
@@ -43,15 +43,10 @@ def predict(image_path):
 
         return x_scale_factor, y_scale_factor
 
-    ##############################################
-    #  Main Inference part
-    ##############################################
-
-    # read image
+    #Lê a imagem usando OpenCV e converte de BGR para RGB.
     im = cv2.imread(image_path)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
-    # unify image channels to 3
     if len(im.shape) == 2:
         im = im[:, :, None]
     if im.shape[2] == 1:
@@ -59,41 +54,40 @@ def predict(image_path):
     elif im.shape[2] == 4:
         im = im[:, :, 0:3]
 
-    # normalize values to scale it between -1 to 1
+    #Normaliza os valores de pixel para o intervalo [-1, 1]
     im = (im - 127.5) / 127.5
 
     im_heigth, im_width, im_c = im.shape
     x, y = get_scale_factor(im_heigth, im_width, ref_size)
-
-    # resize image
     im = cv2.resize(im, None, fx = x, fy = y, interpolation = cv2.INTER_AREA)
 
-    # prepare input shape
+    #Faz a imagem ser transposta
     im = np.transpose(im)
+    #Troca os eixos da imagem
     im = np.swapaxes(im, 1, 2)
+    #Adiciona uma nova dimensão à imagem no eixo = 0
     im = np.expand_dims(im, axis = 0).astype('float32')
 
-    # Initialize session and get prediction
+    #Cria uma sessão de inferencia
     session = onnxruntime.InferenceSession(model_path, None)
     input_name = session.get_inputs()[0].name
     output_name = session.get_outputs()[0].name
     result = session.run([output_name], {input_name: im})
 
-    # refine matte
+    #Converte o resultado da inferência para o formato de imagem e salva a saída.
     matte = (np.squeeze(result[0]) * 255).astype('uint8')
     matte = cv2.resize(matte, dsize=(im_width, im_heigth), interpolation = cv2.INTER_AREA)
 
     cv2.imwrite(output_path, matte)
 
-    ##############################################
-    # Optional - save png image without background
-    ##############################################
-
     im_PIL = Image.open(image_path)
+    #Converte um array NumPy em um objeto de imagem
     matte = Image.fromarray(matte)
-    im_PIL.putalpha(matte)   # add alpha channel to keep transparency
+    #Adiciona o canal alfa(transparencia)
+    im_PIL.putalpha(matte)
     im_PIL.save('static/uploads/detected.png')
 
+    #Define os tamanhos da imagem para fazer a sobreposição de forma correta
     def resize_image(image_path, new_size, by_width):
         im = Image.open(image_path)
 
@@ -109,20 +103,19 @@ def predict(image_path):
 
         resized.save(image_path)
 
-
+    #Função para sobreposição da imagem recortada no novo bakcground
     def adjust_images(imgs):
         open_im = [Image.open(image) for image in imgs]
 
         im_base_h, im_base_w = open_im[0].size
         im_sup_h, im_sup_w = open_im[1].size
 
-
         if im_sup_w > im_base_w:
             resize_image(imgs[1], im_base_w, False)
         if im_sup_h > im_base_h:
             resize_image(imgs[0], im_sup_h, True)
 
-
+    #Junção da imagem recortada no seu novo background
     def merge_images(imgs):
         open_im = [Image.open(img).convert("RGBA") for img in imgs]
 
@@ -136,11 +129,11 @@ def predict(image_path):
         x_center = (im_base_w - open_im[1].width) // 2
         y_center = (im_base_h - open_im[1].height)
 
-
         result.paste(open_im[0], (0, 0), open_im[0])
         result.paste(open_im[1], (x_center, y_center), open_im[1])
 
         result.save(exit_path)
+
 
     im_base_path = "static/uploads/background.png"
     im_sub_path = "static/uploads/detected.png"
